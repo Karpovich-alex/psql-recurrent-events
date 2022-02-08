0e39d693223372d9cc1f96b7d21e4494bec26475
Karpo <karpovehalex@gmail.com>
2022-02-08 17:24:54 +0300

    Change insert method for parameters

--- add_event.sql
+++ add_event.sql
@@ -1,5 +1,7 @@
-DROP FUNCTION add_event(integer, integer, text, text, date, date, time without time zone, time without time zone,
-                        text[]);
+DROP FUNCTION IF EXISTS add_event(integer, integer, text, text, date, date, time without time zone,
+                                  time without time zone,
+                                  text[]);
+-- Add event and returns new event id
 CREATE OR REPLACE FUNCTION add_event(
     user_id integer,
     calendar_id integer,
@@ -9,18 +11,17 @@ CREATE OR REPLACE FUNCTION add_event(
     end_date date,
     start_time time,
     end_time time,
-    params text[][]
+    params json
 )
-    RETURNS VOID
+    RETURNS integer
     LANGUAGE plpgsql
 AS
 $Body$
 DECLARE
-    calendar_ids  users_calendar.calendar_id%type;
-    event_id      event.id%type := -1;
-    param         parameters%rowtype;
-    param_counter integer       := 1;
+    calendar_ids users_calendar.calendar_id%type;
+    event_id     event.id%type := -1;
 BEGIN
+    -- Проверяем доступность календаря пользователю
     SELECT uc.calendar_id
     INTO calendar_ids
     from users
@@ -29,9 +30,10 @@ BEGIN
       and uc.calendar_id = $2;
     IF not FOUND
     THEN
-        RAISE NOTICE 'Calendar id doesnt exist';
+        RAISE EXCEPTION 'User with id: % doesnt have Calendar with id %', user_id, calendar_id;
     ELSE
         RAISE NOTICE 'Calendar %  exists', calendar_ids;
+        -- Добавляем событие в календарь
         INSERT INTO event (calendar_id, title, description, start_date, end_date, start_time, end_time)
         VALUES (calendar_id,
                 e_title,
@@ -43,23 +45,30 @@ BEGIN
         RETURNING id INTO event_id;
         RAISE NOTICE 'Event % inserted', event_id;
         RAISE NOTICE 'Params: %', params;
-        RAISE NOTICE '_______________';
-        FOR param in SELECT * FROM parameters as p WHERE p.name = ANY (params)
-            LOOP
-                RAISE NOTICE 'Params info: %', param.id;
-                RAISE NOTICE 'Params info input: %', params[param_counter][2];
-                RAISE NOTICE '_______________';
-                INSERT INTO pattern (event_id, calendar_id, parameter_id, parameter_value)
-                VALUES (event_id,
-                        calendar_id,
-                        param.id,
-                        params[param_counter][2]);
-                param_counter = param_counter + 1;
-            end loop;
+        -- Добавляем параметры к событию
+        INSERT INTO pattern
+        SELECT event_id, calendar_id, id, value #>> '{}'
+        FROM parameters as p
+                 JOIN json_each(params) params ON params.key = p.name;
+
+        RETURN event_id;
     END IF;
 END;
 $Body$;
 end;
+
+-- Ошибка, так как у пользователя нет такого календаря!
+SELECT *
+from add_event(1, 2, 'New event', 'Event has added from function',
+               '2022-03-01'::date,
+               '2022-03-06'::date,
+               '13:00'::time,
+               '13:30'::time,
+               '{
+                 "frequency": "2",
+                 "count": "2",
+                 "interval": "3"
+               }'::json);
 
 SELECT *
 from add_event(1, 1, 'New event', 'Event has added from function',
@@ -67,11 +76,20 @@ from add_event(1, 1, 'New event', 'Event has added from function',
                '2022-03-06'::date,
                '13:00'::time,
                '13:30'::time,
-               ARRAY [['frequency', '2'],['count', '3'],['interval', '3']]);
+               '{
+                 "frequency": "2",
+                 "count": "2",
+                 "interval": "3",
+                 "new rule": "1000"
+               }'::json);
 SELECT *
 from add_event(1, 1, 'Second event', 'Event has added from function x2',
                '2021-03-01'::date,
                '2021-03-06'::date,
                '09:00'::time,
                '12:30'::time,
-               ARRAY [['until', '2021-03-06'],['count', '3'],['interval', '1']]);
+               '{
+                 "until": "2021-03-06",
+                 "count": "3",
+                 "interval": "1"
+               }'::json);
