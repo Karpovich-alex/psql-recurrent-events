6cb6e45ea69cb1eb06ce316f267e7b0a3a28697f
Karpo <karpovehalex@gmail.com>
2022-03-13 23:54:36 +0300

    Add delete event

---
+++ event/delete_event.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION delete_event(user_id int, calendar_id int, event_id int)
+    RETURNS integer
+    LANGUAGE plpgsql
+AS
+$Body$
+DECLARE
+BEGIN
+    -- Check if a calendar is available to a user
+    PERFORM check_user_calendar(user_id, calendar_id);
+    -- Delete event
+    DELETE FROM pattern pat WHERE pat.event_id = $3;
+    DELETE FROM event ev WHERE ev.id = $3;
+
+
+    RETURN event_id;
+END;
+$Body$;
+end;
---
+++ event/test/test_delete_events.sql
@@ -0,0 +1,4 @@
+-- Error
+SELECT delete_event(1,2,3);
+
+SELECT delete_event(1,1,1);
