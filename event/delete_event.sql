d34d85b07a7364af3fddf3f529e9efb6023bcd5b
Karpo <karpovehalex@gmail.com>
2022-03-20 22:25:43 +0300

    Fix delete event function

--- event/delete_event.sql
+++ event/delete_event.sql
@@ -7,11 +7,30 @@ DECLARE
 BEGIN
     -- Check if a calendar is available to a user
     PERFORM check_user_calendar(user_id, calendar_id);
-    -- Delete event
+    -- Delete pattern
     DELETE FROM pattern pat WHERE pat.event_id = $3;
+    -- Delete exception events
+    DELETE FROM exception_event ex_e WHERE ex_e.calendar_id = $2 AND ex_e.event_id = $3;
+    -- Delete event
     DELETE FROM event ev WHERE ev.id = $3;
-
     RETURN event_id;
+END;
+$Body$;
+end;
+
+CREATE OR REPLACE FUNCTION delete_exception_event(user_id int, calendar_id int, exception_event_id int)
+    RETURNS integer
+    LANGUAGE plpgsql
+AS
+$Body$
+DECLARE
+BEGIN
+    -- Check if a calendar is available to a user
+    PERFORM check_user_calendar(user_id, calendar_id);
+    -- Delete exception events
+    DELETE FROM exception_event ex_e WHERE ex_e.calendar_id = $2 AND ex_e.id = $3;
+
+    RETURN exception_event_id;
 END;
 $Body$;
 end;
